30b6ef70721f4b08045ca198240e8c748ad4a7b8
mszawerd <01169246@pw.edu.pl>
2023-12-15 09:39:23 +0100

    add doc

--- zad23/analyze.py
+++ zad23/analyze.py
@@ -10,7 +10,8 @@ for filename in os.listdir(current_directory):
 min_size = min([len(df) for (fn, df) in dfs])
 for filename, df in dfs:
     df = df.head(min_size)
+    print(filename)
     print(df.describe())
-    fig = df.plot().get_figure()
+    fig = df.plot(xlabel="# pakiety", ylabel="Czas wykonania send", title="Wykres czasu wysłania od zdarzeń wysłania").get_figure()
     fig.set_size_inches(18.5, 10.5)
     fig.savefig(filename+"-plot.png")
